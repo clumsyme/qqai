39279b576f9ab908bb0eae993c6eb5c8494369df
LiYan <lidoyan@gmail.com>
2019-01-18 11:36:42 +0800

    NLPTrans export

--- qqai/__init__.py
+++ qqai/__init__.py
@@ -1,6 +1,6 @@
-__all__ = ['nlp', 'aai', 'vision', 'Detectface', 'TextChat', 'ImgToText']
+__all__ = ['nlp', 'aai', 'vision', 'Detectface', 'TextChat', 'ImgToText', 'NLPTrans']
 
 import qqai.nlp
 import qqai.aai
 import qqai.vision
-from qqai.qqai import Detectface, TextChat, ImgToText
+from qqai.qqai import Detectface, TextChat, ImgToText, NLPTrans
